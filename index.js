8418beef57e5526e14771c835f9d64c0741d2c8b
ahamedsiammia <md7559263@gmail.com>
2026-04-10 16:20:00 +0600

    recap payment api

--- index.js
+++ index.js
@@ -61,35 +61,69 @@ async function run() {
     });
     // payment get way work start
 
+    app.post("/payment-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100 ;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          { 
+            price_data:{
+              currency:"USD",
+              unit_amount: amount,
+              product_data:{
+                name: `Please pay for ${paymentInfo.parcelName}`
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        metadata:{
+          parcelId : paymentInfo.parcelId
+        },
+        customer_email: paymentInfo.senderEmail,
+        success_url: `${process.env.SETE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.SETE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+      console.log(session);
+      res.send({url: session.url});
+    });
+
+    // old
     app.post("/create-checkout-session", async (req, res) => {
       const paymentInfo = req.body;
       const amount = parseInt(paymentInfo.cost) * 100;
       const session = await stripe.checkout.sessions.create({
         line_items: [
           {
-            price_data:{
-                currency:"USD",
-                unit_amount: amount,
-                product_data: {
-                  name :paymentInfo.parcelName,
-
-                }
+            price_data: {
+              currency: "USD",
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.parcelName,
+              },
             },
             quantity: 1,
           },
         ],
         customer_email: paymentInfo.senderEmail,
         mode: "payment",
-        metadata:{
-          parcelId : paymentInfo.parcelId
+        metadata: {
+          parcelId: paymentInfo.parcelId,
         },
         success_url: `${process.env.SETE_DOMAIN}/dashboard/payment-success`,
         cancel_url: `${process.env.SETE_DOMAIN}/dashboard/payment-cancelled`,
       });
 
       console.log(session);
-      res.send({url: session.url})
+      res.send({ url: session.url });
     });
+
+    app.patch("/payment-success", async(req,res)=>{
+      const sessionId = req.query.session_id;
+      console.log("session id ", sessionId);
+      res.send({success: true})
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
